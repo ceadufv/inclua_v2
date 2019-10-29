6e1cc9becfbafbbd5a9b3c2657c045821ba6b0ba
ricsonl <ricsonluizv@gmail.com>
2019-10-29 11:59:33 -0300

    tradução

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'inclua_v2' );
 define( 'DB_USER', 'root' );
 
 /** Senha do banco de dados MySQL */
-define( 'DB_PASSWORD', '123456' );
+define( 'DB_PASSWORD', '' );
 
 /** Nome do host do MySQL */
 define( 'DB_HOST', 'localhost' );
